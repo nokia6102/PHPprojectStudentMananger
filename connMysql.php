c349b06d2da68f8bee687daebffb0e8b453d0673
MaxCheng <nokia6102@gmail.com>
2017-07-27 08:52:27 +0800

    First Commit for classmate Version.

---
+++ connMysql.php
@@ -0,0 +1,33 @@
+<?php 
+
+$servername="localhost";
+$username="root";
+$password="12345";
+$database="class1";
+$port=6033;
+
+//creat connection
+$conn=mysqli_connect($servername,$username,$password,$database,$port);
+
+//check connection
+if (mysqli_connect_error()) {
+    print "failed to connect to mysql:" . mysqli_connect_error();
+}else{
+    //print "connect successfully<br>";
+}
+
+mysqli_query($conn, "SET NAMES utf8");
+
+//.....以上為固定使用的語法
+
+
+
+
+
+
+
+
+
+
+
+ ?>
